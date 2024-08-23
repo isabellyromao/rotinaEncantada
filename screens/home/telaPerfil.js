ff6d2e47725c9d882d4a15e2225d8dbc85d95382
isabellyromao <bellsrom3@gmail.com>
2024-08-23 13:29:05 -0300

    tentativa de criar o deletar conta

--- screens/home/telaPerfil.js
+++ screens/home/telaPerfil.js
@@ -3,9 +3,9 @@ import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
 import { router } from "expo-router";
 import { View, Alert, Text, ScrollView, Image, TextInput, Pressable, StyleSheet, SafeAreaView } from "react-native";
-import { signOut } from "firebase/auth"
+import { getAuth, signOut, deleteUser, EmailAuthProvider, reauthenticateWithCredential  } from "firebase/auth"
 import { auth, db } from '../../firebaseConfig'
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc, deleteDoc } from "firebase/firestore";
 import { BotaoPrincipal, SetaVoltar, CampoDataDeNascimento, CampoGenero } from "../../componentes/geral";
 import { Pompiere_400Regular } from "@expo-google-fonts/pompiere";
 import { NotoSans_600SemiBold } from "@expo-google-fonts/noto-sans";
@@ -15,18 +15,18 @@ export default function TelaPerfil() {
     const [editar, setEditar] = useState(false);
     const [userData, setUserData] = useState({nome: '', dataNascimento: '', genero: ''});
 
-    useEffect(() => {
-        const fetchUserDado = async () => {
-            const user = auth.currentUser;
-            if (user) {
-                const userDoc = await getDoc(doc(db, 'usuarios', user.uid));
-                if (userDoc.exists()) {
-                    const data = userDoc.data();
-                    setUserData(data);
-                }
+    const fetchUserDado = async () => {
+        const user = auth.currentUser;
+        if (user) {
+            const userDoc = await getDoc(doc(db, 'usuarios', user.uid));
+            if (userDoc.exists()) {
+                const data = userDoc.data();
+                setUserData(data);
             }
-        };
+        }
+    };
 
+    useEffect(() => {
         fetchUserDado();
     }, []);
 
@@ -40,49 +40,65 @@ export default function TelaPerfil() {
         return idade;
     };
 
+
     const handleAtualizar = async () => {
         if (editar) {
             // Se estiver em modo de edição e o usuário clicar em SALVAR
-            try {
-                // Verificação de campos
-                if (!userData.nome.trim()) {
-                    Alert.alert("Erro", "Por favor, insira seu nome completo.");
-                    return;
-                }
-                const nomeParts = userData.nome.trim().split(' ');
-                if (nomeParts.length < 2) {
-                    Alert.alert("Erro", "Por favor, insira seu nome completo (nome e sobrenome).");
-                    return;
-                }
-                if (!userData.dataNascimento) {
-                    Alert.alert("Erro", "Por favor, insira sua data de nascimento.");
-                    return;
-                }
-                if (!userData.genero) {
-                    Alert.alert("Erro", "Por favor, selecione seu gênero.");
-                    return;
-                }
-
-                const idade = calcularIdade(userData.dataNascimento);
-                if (idade < 15) {
-                    Alert.alert("Erro", "Você precisa ter pelo menos 15 anos para se cadastrar.");
-                    return;
-                }    
+            Alert.alert(
+                "Confirmar Alterações", "Você tem certeza que deseja salvar as alterações?",
+                [
+                    {
+                        text: "Cancelar",
+                        onPress: () => console.log("Alterações canceladas"),
+                        style: "cancel"
+                    },
+                    {
+                        text: "Salvar",
+                        onPress: async () => {
+                            try {
+                                // Verificação de campos
+                                if (!userData.nome.trim()) {
+                                    Alert.alert("Erro", "Por favor, insira seu nome completo.");
+                                    return;
+                                }
+                                const nomeParts = userData.nome.trim().split(' ');
+                                if (nomeParts.length < 2) {
+                                    Alert.alert("Erro", "Por favor, insira seu nome completo (nome e sobrenome).");
+                                    return;
+                                }
+                                if (!userData.dataNascimento) {
+                                    Alert.alert("Erro", "Por favor, insira sua data de nascimento.");
+                                    return;
+                                }
+                                if (!userData.genero) {
+                                    Alert.alert("Erro", "Por favor, selecione seu gênero.");
+                                    return;
+                                }
     
-                 // Atualizar dados no Firestore
-                 const user = auth.currentUser;
-                 if (user) {
-                     await setDoc(doc(db, 'usuarios', user.uid), userData, { merge: true });
-                     
-                     Alert.alert("Sucesso", "Dados atualizados com sucesso!");
-                     setEditar(false);  // Desativa o modo de edição após salvar
-                 }
+                                const idade = calcularIdade(userData.dataNascimento);
+                                if (idade < 15) {
+                                    Alert.alert("Erro", "Você precisa ter pelo menos 15 anos para se cadastrar.");
+                                    return;
+                                }
     
-            } catch (error) {
-                console.error(error.code);
-                console.error(error.message);
-                Alert.alert("Erro", "Ocorreu um erro ao atualizar os dados.");
-            }
+                                // Atualizar dados no Firestore
+                                const user = auth.currentUser;
+                                if (user) {
+                                    await setDoc(doc(db, 'usuarios', user.uid), userData, { merge: true });
+                                    
+                                    Alert.alert("Sucesso", "Dados atualizados com sucesso!");
+                                    setEditar(false);  // Desativa o modo de edição após salvar
+                                }
+    
+                            } catch (error) {
+                                console.error(error.code);
+                                console.error(error.message);
+                                Alert.alert("Erro", "Ocorreu um erro ao atualizar os dados.");
+                            }
+                        }
+                    }
+                ]
+            );
         } else {
             // Se estiver em modo de visualização e o usuário clicar em EDITAR
             setEditar(true);  // Ativa o modo de edição
@@ -98,6 +114,58 @@ export default function TelaPerfil() {
             console.error(error.message);
         }
     }
+
+
+    const handleDeletarConta = async () => {
+        
+        const user = auth.currentUser;
+
+        if (!user) {
+            Alert.alert("Erro", "Usuário não autenticado.");
+            return;
+        }
+
+        const email = user.email; 
+        const senha = user.senha
+
+        Alert.alert("Excluir Conta","Tem certeza que deseja excluir sua conta? Esta ação não pode ser desfeita.",
+            [
+                {
+                    text: "Cancelar",
+                    style: "cancel",
+                },
+                {
+                    text: "Excluir",
+                    onPress: async () => {
+                        try {
+                            // Solicitar reautenticação, se necessário
+                            const credential = EmailAuthProvider.credential(email, senha);
+                            await reauthenticateWithCredential(user, credential);
+
+                            // Deletar a conta do Firebase Auth
+                            await deleteUser(user);
+
+                            // Deletar dados do Firestore
+                            await deleteDoc(doc(db, 'usuarios', user.uid));
+
+                            Alert.alert("Sucesso", "Sua conta foi excluída.");
+                            router.replace('/'); 
+
+                        } catch (error) {
+                            console.error(error.code);
+                            console.error(error.message);
+
+                            if (error.code === 'auth/requires-recent-login') {
+                                Alert.alert("Erro", "Por favor, faça login novamente para excluir sua conta.");
+                            } else {
+                                Alert.alert("Erro", "Não foi possível excluir sua conta.");
+                            }
+                        }
+                    },
+                },
+            ],
+        );
+    };
 
     let [fonteCarregada, fonteErro] = useFonts({
         Pompiere_400Regular,
@@ -168,6 +236,16 @@ export default function TelaPerfil() {
                             width={188}
                             onPress={handleAtualizar}
                         />
+                        {editar && (
+                        <BotaoSecundario
+                            titulo="CANCELAR"
+                            onPress={() => {
+                            // Reverter os dados para os valores iniciais
+                            fetchUserDado();
+                            setEditar(false); // Desativar o modo de edição
+                            }}
+                        />
+                        )}
                     </View>
 
                     <View style={[styles.fundoDados]}>
@@ -179,9 +257,9 @@ export default function TelaPerfil() {
                     </View>
                     <View style={styles.espacamentoBotoes}>
                         <BotaoSecundario titulo="DESCONECTAR CONTA" onPress={handleSair} />
-                        <BotaoSecundario titulo="DESCONECTAR CONTA" onPress={handleSair} />
+                        <Image source={require("../../assets/detalhe-estrela.png")} style={styles.imagemDetalheEstrela} />
+                        <BotaoPrincipal titulo="DELETAR CONTA" onPress={handleDeletarConta}/>
                     </View>
-                    <Image source={require("../../assets/detalhe-estrela.png")} style={styles.imagemDetalheEstrela} />
                 </ScrollView>
             </View>
         </SafeAreaView>
@@ -259,8 +337,8 @@ const styles = StyleSheet.create({
         color: "#000000"
     },
     espacamentoBotoes: {
-        paddingVertical: 20,
-        gap: 20
+        gap: 30,
+        alignItems:"center"
     },
     imagemDetalheEstrela: {
         width: 35,
